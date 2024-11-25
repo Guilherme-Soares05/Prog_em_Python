2167f90bdf2ede0c253ad5d5c6142021624cb426
Guilherme Soares <95151247+Guilherme-Soares05@users.noreply.github.com>
2024-11-25 23:50:57 +0000

    Criei o arquivo Ex1 que tem 3 tipo de variáveis e exibe o valor delas com print

---
+++ Exercicios/Ex1.py
@@ -0,0 +1,9 @@
+#Nome recebe um valor do tipo String
+nome = "Guilherme"
+#Idade recebe um valor do tipo numérico
+idade = 21
+#Peso recebe um valor do tipo Float (número flutuante)
+peso = 86.9
+print (nome)
+print (idade)
+print (peso)
