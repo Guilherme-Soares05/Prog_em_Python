39b54279524bab86d06d6b3825bacf5744ab431c
Guilherme Soares <95151247+Guilherme-Soares05@users.noreply.github.com>
2024-11-26 22:55:36 +0000

    No Exercicio 3 foi utilizado as 6 operações e foi apresentada as mesmas com um texto dentro do print

---
+++ Exercicios/Ex3.py
@@ -0,0 +1,15 @@
+soma = 1 + 1
+multiplicacao = 4 * 4
+divisao = 4 / 3
+subtracao = 50-28
+potencia = 4 ** 4
+porcentagem = 10 % 100
+
+##Para imprimir um texto e mais o resultado de uma variável junto é necessário colocar a virgula após as aspas
+
+print ("A soma é igual a: ", soma)
+print ("A multiplicação é igual a: ", multiplicacao)
+print ("A divisão é igual a: ", divisao)
+print ("A subtração é igual a: ", subtracao)
+print ("A potência é igual a: ", potencia)
+print ("A porcentagem é igual a: ", porcentagem)
